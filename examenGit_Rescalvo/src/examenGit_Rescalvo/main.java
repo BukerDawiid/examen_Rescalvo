f401f343d855f2d6eb9fe777941298f760c0d3a1
BukerDawiid <36193361+BukerDawiid@users.noreply.github.com>
2018-02-27 09:54:00 +0100

    segon commit

--- examenGit_Rescalvo/src/examenGit_Rescalvo/main.java
+++ examenGit_Rescalvo/src/examenGit_Rescalvo/main.java
@@ -5,16 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
 	}
 	
 	public static void printOne() {
 		System.out.println("hello world");
 	}
-	
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
-
 }
